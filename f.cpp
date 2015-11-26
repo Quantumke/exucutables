5d50ab75af071bb528b54f126e5df1b52a1f41a1
Quantumke <nguruben@gmail.com>
2015-11-26 19:33:38 +0300

    Loop through

---
+++ f.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main(){
+for(int x =5; x>0; x--)
+{
+cout<<x;
+}
+}
---
+++ switch.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+int num;
+cout<<"Enter a number:";
+cin>>num;
+switch(num)
+{
+case 10:
+cout<< "Value is 10";
+break;
+case 20:
+cout<<"Value is 20";
+break;
+default:
+cout<<"Invalid input";
+}
+}
